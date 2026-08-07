7d53dce6392561b3454b8813c55c3a98db9e58f9
JaaJ <joepqnk@gmail.com>
2026-08-07 17:33:20 +0200

    feature (user): WIP sur l'architecture centrée sur User

---
+++ ticketflow/src/main/java/com/jaajou/ticketflow/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.jaajou.ticketflow.repository;
+
+import java.util.Collection;
+import com.jaajou.ticketflow.domain.User;
+
+public interface UserRepository<T extends User> {
+    /*Basic CRUD Operations*/
+    T create(T data);
+    Collection<T> list(int page, int pageSize);
+    T get(Long id);
+    T update(T data);
+    void delete(T data);
+    Boolean delete (Long id);
+
+    /*More complex Operations */
+}
---
+++ ticketflow/src/main/java/com/jaajou/ticketflow/repository/implementation/UserRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.jaajou.ticketflow.repository.implementation;
+
+import com.jaajou.ticketflow.domain.User;
+import com.jaajou.ticketflow.repository.UserRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserRepositoryImpl implements UserRepository<User> {
+
+    @Override
+    public User create(User data) {
+        return null;
+    }
+
+    @Override
+    public Collection<User> list(int page, int pageSize) {
+        return List.of();
+    }
+
+    @Override
+    public User get(Long id) {
+        return null;
+    }
+
+    @Override
+    public User update(User data) {
+        return null;
+    }
+
+    @Override
+    public void delete(User data) {
+
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
