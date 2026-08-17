1852724330d5bb3182eefc2b653990d7fb0d0a38
JaaJou <joepqnk@gmail.com>
2026-08-17 10:14:50 +0200

    bdd: modification du fichier de cration d'un role pour eviter des problemes de connexions lors du premier lancement

--- ticketflow/src/main/resources/db/admin/00_create_role.sql
+++ ticketflow/src/main/resources/db/admin/00_create_role.sql
@@ -1,25 +1,59 @@
-/******************************************************************************
- * Création de l'utilisateur PostgreSQL de l'application TicketFlow
- * À exécuter une seule fois avec un compte administrateur PostgreSQL.
- ******************************************************************************/
+-- ============================================================
+-- Création du rôle applicatif
+-- ============================================================
 
--- Création du rôle
 CREATE ROLE ticketflow_app
-WITH LOGIN
-PASSWORD 'ticketflow_mdp';
+    WITH
+    LOGIN
+    PASSWORD 'ticketflow_mdp';
 
--- Autorisation de connexion à la base
+-- ============================================================
+-- Création de la base
+-- ============================================================
+
+CREATE DATABASE ticketflow
+    OWNER ticketflow_app;
+
+-- ============================================================
+-- Connexion à la base ticketflow
+-- ============================================================
+-- À exécuter ensuite dans la base ticketflow
+
+CREATE SCHEMA IF NOT EXISTS ticketflow
+    AUTHORIZATION ticketflow_app;
+
 GRANT CONNECT ON DATABASE ticketflow TO ticketflow_app;
+GRANT CREATE ON DATABASE ticketflow TO ticketflow_app;
 
--- Autorisation d'utiliser le schéma
-GRANT USAGE ON SCHEMA ticketflow TO ticketflow_app;
+GRANT USAGE, CREATE
+ON SCHEMA ticketflow
+TO ticketflow_app;
 
--- Droits sur toutes les tables existantes
-GRANT SELECT, INSERT, UPDATE, DELETE
+-- Objets existants
+GRANT ALL PRIVILEGES
 ON ALL TABLES IN SCHEMA ticketflow
 TO ticketflow_app;
 
--- Droits sur les séquences (IDENTITY)
-GRANT USAGE, SELECT
+GRANT ALL PRIVILEGES
 ON ALL SEQUENCES IN SCHEMA ticketflow
+TO ticketflow_app;
+
+GRANT ALL PRIVILEGES
+ON ALL FUNCTIONS IN SCHEMA ticketflow
+TO ticketflow_app;
+
+-- Objets futurs
+ALTER DEFAULT PRIVILEGES
+IN SCHEMA ticketflow
+GRANT ALL PRIVILEGES ON TABLES
+TO ticketflow_app;
+
+ALTER DEFAULT PRIVILEGES
+IN SCHEMA ticketflow
+GRANT ALL PRIVILEGES ON SEQUENCES
+TO ticketflow_app;
+
+ALTER DEFAULT PRIVILEGES
+IN SCHEMA ticketflow
+GRANT ALL PRIVILEGES ON FUNCTIONS
 TO ticketflow_app;
